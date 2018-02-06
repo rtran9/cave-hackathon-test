d2eb427b3054736e52c0053349146aa485518e19
robert <rtran@mit.edu>
2018-02-06 15:33:10 -0500

    fix port

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Hello World! Cave Lab Hackathon Test.'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
